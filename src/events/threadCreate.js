7fdb69330def9fcee1ac20514d1239aa9947ac97
Couleur <82747632+couleurm@users.noreply.github.com>
2024-03-02 23:32:49 +0100

    add patterns, adjust threadCreate to use it & OCR

--- src/events/threadCreate.js
+++ src/events/threadCreate.js
@@ -1,20 +1,40 @@
-const { Events } = require('discord.js');
+const { Events, ThreadOnlyChannel } = require('discord.js');
 require('dotenv').config();
+const { createWorker } = require('tesseract.js');
 const supportChannels = require('../../support-channels.json');
+const supportPatterns = require('../../support-patterns.json');
 
 module.exports = {
-	name: Events.ThreadCreate,
+	name: Events.ThreadCreate, // https://discord.js.org/docs/packages/discord.js/main/ThreadChannel:Class
 	async execute(thread) {
-		if(!supportChannels.includes(thread.parentId)) return; // Checks if the channel ID of the thread is in your JSON list
-        
-        /* Your code here...
+		if (!supportChannels.includes(thread.parentId))
+			// if so, it is not a support channel we selected 
+			return;
 
-        'thread' is a discord ThreadChannel, you can see all the properties and methods here:
-        https://discord.js.org/docs/packages/discord.js/14.14.1/ThreadChannel:Class
+		const lastMessage = await thread.fetchStarterMessage();
 
-        For example, thread.send("hello :D");
+		let context = lastMessage.cleanContent
 
-        Most of the docs are well written but let me know if you have questions :) */
+		if (lastMessage.attachments.length > 0) {
 
+			image = lastMessage.attachments.first()
+			thread.send("mmh.. okay.. an image.. lemme analyze it :eye::eye:");
+			const worker = await createWorker('eng');
+
+			const res = await worker.recognize(image.proxyURL);
+			thread.send("done!!");
+
+			context += "\n" + res.data.text
+		}
+
+		// thread.send("```\n" + context + "\n```\n")
+
+		supportPatterns.forEach(problem => {
+			// thread.send(regex.toString() + ': ' + regex.test(context))
+			// thread.send(JSON.stringify(problem))
+
+			const regex = new RegExp(problem.pattern, "gis")
+			if (regex.test(context)) thread.send(problem.response);
+		})
 	},
 };
